14842625bbd77f23060bd39aa870fdff40d878cb
revathilakshmanan <77661388+revathilakshmanan@users.noreply.github.com>
2021-05-08 15:09:34 +0530

    Update api.service.ts

--- src/app/core/api.service.ts
+++ src/app/core/api.service.ts
@@ -8,11 +8,11 @@ import {ApiResponse} from "../model/api.response";
 export class ApiService {
 
   constructor(private http: HttpClient) { }
-  baseUrlUser: string = 'http://192.168.56.101:9910/users/';
-  baseUrlClaim: string = 'http://192.168.56.101:9910/claims/';
+  baseUrlUser: string = 'http://152.70.67.178:32001/users/';
+  baseUrlClaim: string = 'http://152.70.67.178:32001/claims/';
 
   login(loginPayload) : Observable<ApiResponse> {
-    return this.http.post<ApiResponse>('http://192.168.56.101:9910/' + 'token/generate-token', loginPayload);
+    return this.http.post<ApiResponse>('http://152.70.67.178:32001/' + 'token/generate-token', loginPayload);
   }
 
   getUsers() : Observable<ApiResponse> {
