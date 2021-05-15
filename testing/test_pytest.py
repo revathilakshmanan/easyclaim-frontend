ea84d2dae51f94f86408a7fb21aed71e6a45c3dd
Vignesh Sweekaran <40670015+vigneshsweekaran@users.noreply.github.com>
2021-05-15 13:42:19 +0530

    Update test_pytest.py

--- testing/test_pytest.py
+++ testing/test_pytest.py
@@ -16,8 +16,8 @@ def test_login():
     driver.implicitly_wait(10)
     driver.get('http://localhost:80')
     driver.maximize_window()
-    driver.find_element_by_id("username").send_keys('vishnu')
-    driver.find_element_by_id('pwd').send_keys('vishnu@123')
+    driver.find_element_by_id("username").send_keys('test')
+    driver.find_element_by_id('pwd').send_keys('test')
     driver.find_element_by_xpath('/html/body/app-root/app-login/div/div/form/button').click()
     print(driver.title)
 def test_title_check():
